665ce3d3a826d4da2a85a1d7bfc7701731b94186
Solkol <61247142+Solkol@users.noreply.github.com>
2023-11-03 16:27:48 +0100

    Add hovering event listener

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let color = 'green';
+
 const gridContainer = document.querySelector('#gridContainer');
 
 for (let i = 0; i < 16; i++) {      //grid creation loop
     const row = document.createElement('div');
     row.classList.add('row');
+    row.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = color;
+    });
 
     for (let j = 0; j < 16; j++) {
         const gridDiv = document.createElement('div');
