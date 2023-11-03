364911d8208432766bb1e589eb42098daabeaa83
Solkol <61247142+Solkol@users.noreply.github.com>
2023-11-03 17:05:02 +0100

    Add createGrid()

--- script.js
+++ script.js
@@ -7,26 +7,31 @@ const gridContainer = document.querySelector('#gridContainer');
 btnSquare.addEventListener('click', () => {
 	squares = Number(prompt('Select number of elements per side of the grid: ', "16"));
     deleteGrid(gridContainer);
+    createGrid(gridContainer);
 });
 
-for (let i = 0; i < squares; i++) {      //grid creation loop
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = color;
-    });
+createGrid(gridContainer);
 
-    for (let j = 0; j < squares; j++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.classList.add('gridElement');
-        row.appendChild(gridDiv);
+function createGrid(grid) {
+    for (let i = 0; i < squares; i++) {      //grid creation loop
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = color;
+        });
+
+        for (let j = 0; j < squares; j++) {
+            const gridDiv = document.createElement('div');
+            gridDiv.classList.add('gridElement');
+            row.appendChild(gridDiv);
+        }
+
+        grid.appendChild(row);
     }
-
-    gridContainer.appendChild(row);
 }
 
-function deleteGrid(gridContainer) {
-	while (gridContainer.firstChild) {
-		gridContainer.removeChild(gridContainer.firstChild);
+function deleteGrid(grid) {
+	while (grid.firstChild) {
+		grid.removeChild(grid.firstChild);
 	}
 }
