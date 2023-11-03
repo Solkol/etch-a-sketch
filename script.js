e369d52f55b3cb1ce07fe03892ecaff72d34b8fd
Solkol <61247142+Solkol@users.noreply.github.com>
2023-11-03 16:46:40 +0100

    Add click event listener to button

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let color = 'green';
+let squares = 16;
+
+const btnSquare = document.querySelector('#btnSquare');
+btnSquare.addEventListener('click', () => {
+	squares = Number(prompt('Select number of elements per side of the grid: ', "16"));
+});
 
 const gridContainer = document.querySelector('#gridContainer');
 
-for (let i = 0; i < 16; i++) {      //grid creation loop
+for (let i = 0; i < squares; i++) {      //grid creation loop
     const row = document.createElement('div');
     row.classList.add('row');
     row.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = color;
     });
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < squares; j++) {
         const gridDiv = document.createElement('div');
         gridDiv.classList.add('gridElement');
         row.appendChild(gridDiv);
